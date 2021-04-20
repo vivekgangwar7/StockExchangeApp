23987519cd9fb6a0259ef80c24aec9ef712bda4a
Vivek Gangwar <vgangwa@amazon.com>
2021-04-20 19:13:19 +0530

    Updated sorted action with time

--- src/main/java/com/navi/stockexchangeapp/module/companystock/CompanyStockComparator.java
+++ src/main/java/com/navi/stockexchangeapp/module/companystock/CompanyStockComparator.java
@@ -14,21 +14,39 @@ public class CompanyStockComparator implements Comparator<CompanyOrder> {
 
     @Override
     public int compare(CompanyOrder order1, CompanyOrder order2) {
+        int order1TimeStamp = companyOrderIntegerTime(order1.getTimeStamp());
+        int order2TimeStamp = companyOrderIntegerTime(order2.getTimeStamp());
         if (orderType == CompanyOrderType.BUY) {
             if (order1.getPrice() < order2.getPrice()) {
                 return 1;
-            } else if (order1.getPrice() > order2.getPrice()) {
-                return -1;
+            } else if (order1.getPrice() == order2.getPrice()) {
+                if (order1TimeStamp > order2TimeStamp) {
+                    return 1;
+                } else if (order1TimeStamp == order2TimeStamp) {
+                    return 0;
+                }
             }
-            return 0;
+            return -1;
         }
         if (orderType == CompanyOrderType.SELL) {
             if (order1.getPrice() > order2.getPrice()) {
                 return 1;
-            } else if (order1.getPrice() < order2.getPrice()) {
-                return -1;
+            } else if (order1.getPrice() == order2.getPrice()) {
+                if (order1TimeStamp > order2TimeStamp) {
+                    return 1;
+                } else if (order1TimeStamp == order2TimeStamp) {
+                    return 0;
+                }
             }
         }
-        return 0;
+        return -1;
+    }
+
+    private int companyOrderIntegerTime(String timeStamp) {
+        int h1 = timeStamp.charAt(0) - '0';
+        int h2 = timeStamp.charAt(1) - '0';
+        int m1 = timeStamp.charAt(3) - '0';
+        int m2 = timeStamp.charAt(4) - '0';
+        return (h1*10+h2)*60+(m1*10+m2);
     }
 }
